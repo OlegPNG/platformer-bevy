01ff8a57f17b647be694d99352778243f3a35f37
Devin <devin24beck@gmail.com>
2023-05-31 13:38:56 -0400

    Init

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("maps/ldtk-test.ldtk"),
+        ..Default::default()
+    });
+}
